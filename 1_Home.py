0682ab7f557abb521cce4f325fffaf2bd74caf5a
Laurival <netolaurival@gmail.com>
2025-03-04 21:23:19 -0300

    Clear Plot

--- 1_Home.py
+++ 1_Home.py
@@ -73,17 +73,3 @@ with st.expander('Asset'):
 with st.expander('Rent Roll'): 
     st.session_state["df"] = load_data( worksheet = 'Rentroll')
     st.write(st.session_state["df"])
-
-
-# Generate Plot
-with st.expander('Generate Plot'): 
-    st.session_state["df"] = load_data( worksheet = 'Rentroll')
-    x_column = st.selectbox('X axis', st.session_state["df"].columns)
-    y_column = st.selectbox('Y axis', st.session_state["df"].columns)
-
-    plot_data_button = st.button('Generate Plot')
-    if plot_data_button:
-        st.bar_chart(st.session_state["df"].set_index(x_column)[y_column])
-    else:
-        st.write('Click on the button to generate the plot')
-
